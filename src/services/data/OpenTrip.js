1427a2e809dc8f70215602f4916d8ffffa9e8877
YanaPrayana <igrputra24@gmail.com>
2026-05-12 22:01:40 +0700

    menambahkan folder untuk data sementara

---
+++ src/services/data/OpenTrip.js
@@ -0,0 +1,32 @@
+// src/services/data/openTripData.js
+
+export const dataOpenTrip = [
+  {
+    id: 1,
+    nama: "Kraton Yogyakarta",
+    harga: "Rp 150.000",
+    gambar: "../src/assets/images/keraton-budaya.png",
+    deskripsi: "Telusuri sejarah dan nilai-nilai luhur di pusat kerajaan Jawa.",
+  },
+  {
+    id: 2,
+    nama: "Desa Wisata Kasongan",
+    harga: "Rp 120.000",
+    gambar: "../src/assets/images/kasongan-budaya.png",
+    deskripsi: "Lihat langsung proses kerajinan gerabah khas Jogja.",
+  },
+  {
+    id: 3,
+    nama: "Saksikan Wayang Kulit",
+    harga: "Rp 200.000",
+    gambar: "../src/assets/images/pertunjukan-wayang.png",
+    deskripsi: "Nikmati pertunjukan wayang kulit dengan dalang berpengalaman.",
+  },
+  {
+    id: 4,
+    nama: "Belajar Membatik",
+    harga: "Rp 175.000",
+    gambar: "../src/assets/images/membatik-budaya.png",
+    deskripsi: "Pengalaman membatik langsung bersama pengrajin lokal.",
+  },
+];
